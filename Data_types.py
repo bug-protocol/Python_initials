41cee0eb86d0cb59a28d3d6d2fc3a58c5fe32d01
bug-protocol <ashutoshm9918@gmail.com>
2026-05-14 14:51:22 +0530

    Data types

---
+++ Data_types.py
@@ -0,0 +1,55 @@
+# integer
+age = 21
+print(age)
+print(type(age))
+
+
+# float
+price = 99.99
+print(price)
+print(type(price))
+
+
+# string
+name = "Steve"
+print(name)
+print(type(name))
+
+
+# boolean
+x = True
+print(x)
+print(type(x))
+
+
+# list
+arr = [1, 2, 3, 4]
+print(arr)
+print(type(arr))
+
+
+# tuple
+cord = (10, 20)
+print(cord)
+print(type(cord))
+
+
+# set
+num = {1, 2, 3, 4}
+print(num)
+print(type(num))
+
+
+# Dictionary
+student = {
+    "name": "Steve",
+    "age": 21
+}
+print(student)
+print(type(student))
+
+
+# None Type
+data = None
+print(data)
+print(type(data))
